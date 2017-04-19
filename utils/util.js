73af59eb235db578bf485e5f97ffee8cfb491c52
unknown <>
2017-04-19 14:25:35 +0800

    wx:key

--- utils/util.js
+++ utils/util.js
@@ -1,18 +1,18 @@
-var formatTime = function(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
-
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
-  
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-var formatNumber = function(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+var formatTime = function( z ) {
+    if (z === undefined) { z = new Date() }
+    var x = z.toString()
+    var zh     = '天一二三四五六'
+    var Year   = x.slice(11,15)
+    var Month  = z.getMonth() + 1
+    var Day    = x.slice(8,10)
+    var Hour   = x.slice(16,18)
+    var Minute = x.slice(19,21)
+    var Second = x.slice(22,24)
+    var Week   = zh[ z.getDay() ]
+    if ( String(Month).length === 1) {
+        Month = '0' + Month
+    }
+    return `${Year}年${Month}月${Day}日 ${Hour}:${Minute}:${Second} 星期${Week}`
 }
 
 module.exports = {
