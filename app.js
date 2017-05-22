25ea20ff2e9ce61a25e7492e24486a23f211f676
大海 <iwangyang@qq.com>
2017-05-22 15:19:42 +0800

    // 全局变量 compass

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 var options = {
     // onLaunch 全局登陆触发一次
     db: {
-        userInfo: null
+        userInfo: null,
+        compass: null
     },
     onLaunch: function () {
-
+        // 开启罗盘
+        wx.startCompass()
     },
     login: function(that) {
         wx.login({
@@ -22,6 +24,28 @@ var options = {
             },
             fail: () => {}
         })
+    },
+    direction: function(du) {
+        // 360 / 8 = 45
+        if (du >= 338 || du < 23) {
+            return '北'
+        } else if (du >= 23 && du < 68) {
+            return '东北'
+        } else if (du >= 68 && du < 113) {
+            return '东'
+        } else if (du >= 113 && du < 158) {
+            return '东南'
+        } else if (du >= 158 && du < 203) {
+            return '南'
+        } else if (du >= 203 && du < 248) {
+            return '西南'
+        } else if (du >= 248 && du < 293) {
+            return '西'
+        }  else if (du >= 293 && du < 338) {
+            return '西北'
+        }  else {
+            return null
+        }
     }
 }
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,53 +1,31 @@
 const log = require('../../ku/js/log.js')
 const app = getApp()
-// 方向
-const direction = function(du) {
-    // 360 / 8 = 45
-    if (du >= 338 || du < 23) {
-        return '北'
-    } else if (du >= 23 && du < 68) {
-        return '东北'
-    } else if (du >= 68 && du < 113) {
-        return '东'
-    } else if (du >= 113 && du < 158) {
-        return '东南'
-    } else if (du >= 158 && du < 203) {
-        return '南'
-    } else if (du >= 203 && du < 248) {
-        return '西南'
-    } else if (du >= 248 && du < 293) {
-        return '西'
-    }  else if (du >= 293 && du < 338) {
-        return '西北'
-    }  else {
-        return null
-    }
-}
 
 Page({
     data: {
-        compass: null,
+        userInfo: null,
+        compass: null
     },
     onLoad: function () {
+        // 登陆
         if (app.db.userInfo === null) {
             app.login(this)
         }
-    },
-    onPullDownRefresh: function() {
-        wx.stopPullDownRefresh()
-    },
-    clickHead: function() {
+        // 方向
         let that = this
-        // 开启罗盘
-        wx.startCompass()
         wx.onCompassChange(function (res) {
             let du = res.direction.toFixed(2)
-            let str = direction(du)
-            if (that.data !== str) {
+            // 全局变量 compass
+            app.db.compass = app.direction(du)
+            let str = app.db.compass
+            if (str !== that.data.compass) {
                 that.setData({
                     compass: str
                 })
             }
         })
+    },
+    onPullDownRefresh: function() {
+        wx.stopPullDownRefresh()
     }
 })
