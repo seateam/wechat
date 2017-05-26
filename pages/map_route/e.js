2b8dcf12c7a1d90265df31659ba0e6ed1107c4fa
大海 <iwangyang@qq.com>
2017-05-26 18:18:00 +0800

    去挥手啊哈

--- pages/map_route/e.js
+++ pages/map_route/e.js
@@ -2,8 +2,21 @@ const log = console.log.bind(console)
 const amapFile = require('../../ku/js/amap-wx.js')
 const config = require('../../ku/js/config.js')
 const db = {
-    marker: '../../ku/img/icecream-07.png',
-    marker_checked: '../../ku/img/icecream-18.png'
+    markers: [{
+        iconPath: '../../ku/img/icecream-07.png',
+        id: 0,
+        latitude:  null,
+        longitude: null,
+        width: 23,
+        height: 33
+    }, {
+        iconPath: '../../ku/img/icecream-18.png',
+        id: 1,
+        latitude: null,
+        longitude: null,
+        width: 24,
+        height: 34
+    }]
 }
 
 let markersData = []
@@ -12,27 +25,20 @@ Page({
         wx.stopPullDownRefresh()
     },
     data: {
-        markers: [{
-            iconPath: db.marker,
-            id: 0,
-            latitude: 39.989643,
-            longitude: 116.481028,
-            width: 23,
-            height: 33
-        }, {
-            iconPath: db.marker_checked,
-            id: 0,
-            latitude: 39.90816,
-            longitude: 116.434446,
-            width: 24,
-            height: 34
-        }],
-        polyline: []
+        // now 30.572269,104.066541
+        // end 30.70775,104.08171
+        markers: [],
+        polyline: [],
+        myAmapFun: null
+    },
+    onReady: function (e) {
+      // 使用 wx.createMapContext 获取 map 上下文
+      this.mapCtx = wx.createMapContext('navi_map')
     },
     onLoad: function() {
         var that = this;
         var key = config.key;
-        var myAmapFun = new amapFile.AMapWX({
+        this.myAmapFun = new amapFile.AMapWX({
             key: key
         })
         // myAmapFun.getRegeo({
@@ -41,7 +47,7 @@ Page({
         //         log(data)
         //     }
         // })
-        this.path()
+        // this.show(myAmapFun)
     },
     // 解析地址
     deLocation: function() {
@@ -61,13 +67,18 @@ Page({
             }
         })
     },
-    show: function(start, end) {
-        myAmapFun.getDrivingRoute({
-            origin: '116.481028,39.989643',
-            destination: '116.434446,39.90816',
+    show: function(that, start, end) {
+        // 反转坐标
+        let deitude = function(itude) {
+            return itude.split(',').reverse().join(',')
+        }
+        that.myAmapFun.getDrivingRoute({
+            origin: deitude(start),
+            destination: deitude(end),
+            // city: '成都',
             success: function(data) {
-                // console.log(data);
                 var points = [];
+                // 路线
                 if (data.paths && data.paths[0] && data.paths[0].steps) {
                     var steps = data.paths[0].steps;
                     for (var i = 0; i < steps.length; i++) {
@@ -80,20 +91,32 @@ Page({
                         }
                     }
                 }
-                that.setData({
-                    polyline: [{
-                        points: points,
-                        color: "#0091ff",
-                        width: 7,
-                        dottedLine: true
-                    }]
-                });
-                // if (data.paths[0] && data.paths[0].distance) {
-                //     log(data.paths[0].distance + '米')
-                // }
-                // if (data.taxi_cost) {
-                //     log('打车约' + Number(data.taxi_cost).toFixed(2)  + '元')
-                // }
+
+                // 长度
+                let rice = 0
+                if (data.paths[0] && data.paths[0].distance) {
+                    rice = data.paths[0].distance
+                    log(rice + '米')
+                }
+
+                // 打车费用
+                if (data.taxi_cost) {
+                    log('打车约' + Number(data.taxi_cost).toFixed(2)  + '元')
+                }
+                if (rice < 350000) {
+                    that.setData({
+                        polyline: [{
+                            points: points,
+                            color: "#0091ff",
+                            width: 7,
+                            dottedLine: true
+                        }]
+                    })
+                    that.marks()
+                } else {
+                    log('超过350km')
+                }
+
             },
             fail: function(info) {
                 console.log(info);
@@ -107,12 +130,31 @@ Page({
                 wx.getLocation({
                     type: 'wgs84',
                     success: function(now) {
+                        let arr = db.markers
+                        arr[0].latitude = now.latitude
+                        arr[0].longitude = now.longitude
+                        arr[1].latitude = end.latitude
+                        arr[1].longitude = end.longitude
+
+                        that.setData({
+                            markers: arr
+                        })
+
                         now = [now.latitude,now.longitude].join(',')
                         end = [end.latitude, end.longitude].join(',')
-                        that.show(now, end)
+                        that.show(that, now, end)
                     }
                 })
+            },
+            fail: function(err) {
+                log(err)
             }
+        })
+    },
+    marks: function() {
+        this.mapCtx.includePoints({
+          padding: [40, 20, 20, 20],
+          points: this.data.markers
         })
     }
 })
