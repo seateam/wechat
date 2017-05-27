f93a33b7771a6bc31a1a6c83dc7d27ae7c434934
大海 <iwangyang@qq.com>
2017-05-27 16:37:32 +0800

    归去归去来兮

--- pages/index/e.js
+++ pages/index/e.js
@@ -2,6 +2,23 @@ const log = console.log.bind(console)
 const app = getApp()
 Page({
     data: {
+        imgUrls: [{
+                img: 'http://i3.mifile.cn/a4/xmad_14950995035103_fhWtH.jpg',
+                text: '平衡车'
+            },
+            {
+                img: 'http://i3.mifile.cn/a4/xmad_1493109150882_opGFm.jpg',
+                text: '眼镜'
+            },
+            {
+                img: 'http://i3.mifile.cn/a4/xmad_14950996440442_FrUIx.jpg',
+                text: '路由'
+            }
+        ],
+        indicatorDots: true,
+        autoplay: false,
+        interval: 4000,
+        duration: 800,
         // 用户
         userInfo: null,
         // 罗盘
